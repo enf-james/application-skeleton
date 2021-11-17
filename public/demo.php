535fa4644437e46badcd6a32ad5ba79e46e9f76d
James <james@enfsolar.com>
2021-11-17 11:41:43 +0000

    -

--- public/demo.php
+++ public/demo.php
@@ -2,7 +2,7 @@
 $html = file_get_contents(__DIR__. '/demo.html');
 
 $r = preg_replace_callback('|src="(?!https?://)([^"]+)"|', function($matches){
-    $url = "https://" . trim($matches[1], '/');
+    $url = "https://" . ltrim($matches[1], '/');
     return "src=\"$url\"";
 }, $html);
 var_dump($r);
